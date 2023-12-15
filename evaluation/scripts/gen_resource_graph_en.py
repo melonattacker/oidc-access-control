6773fa14ce0d468adf56ae70f36bc3fa49fea22e
melonattacker <junkitombob@gmail.com>
2023-12-15 15:39:12 +0900

    ADD: gen english version graph

---
+++ evaluation/scripts/gen_resource_graph_en.py
@@ -0,0 +1,52 @@
+import csv
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+import statistics
+
+def calc_avg_and_std(filename, key):
+    values = []
+    with open(filename, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            values.append(float(row[key]))
+    return statistics.mean(values), statistics.stdev(values)
+
+def plot_and_save_graph(labels, values, errors, y_label, filename, legend_labels):
+    plt.tick_params(labelsize=12)
+    bars = plt.bar(labels, values, color=['gainsboro', 'darkgray', 'lightgray', 'grey', 'dimgray'], yerr=errors, capsize=5)
+    for bar, legend_label in zip(bars, legend_labels):
+        bar.set_label(legend_label)
+    plt.ylabel(y_label, fontsize=14)
+    plt.legend()
+    plt.savefig(filename, format="png", dpi=300)
+    plt.clf()
+
+# CSVファイルのパス
+baseline_signin_csv = "./evaluation/data/performance/resource/baseline/resource_signin.csv"
+proposed_signin_csv = "./evaluation/data/performance/resource/proposed/resource_signin.csv"
+baseline_after_signin_csv = "./evaluation/data/performance/resource/baseline/resource_after_signin.csv"
+proposed_after_signin_csv = "./evaluation/data/performance/resource/proposed/resource_after_signin.csv"
+proposed_after_signin_confidential_csv = "./evaluation/data/performance/resource/proposed/resource_after_signin_confidential.csv"
+
+# CSVファイルからデータを計算
+labels = ['Before 1', 'After 1', 'Before 2', 'After 2', 'After 3']
+cpu_values = []
+cpu_errors = []
+mem_values = []
+mem_errors = []
+
+for csv_file in [baseline_signin_csv, proposed_signin_csv, baseline_after_signin_csv, proposed_after_signin_csv, proposed_after_signin_confidential_csv]:
+    avg_cpu, std_cpu = calc_avg_and_std(csv_file, 'CPU Usage (%)')
+    avg_mem, std_mem = calc_avg_and_std(csv_file, 'Memory Usage (MB)')
+    cpu_values.append(avg_cpu)
+    cpu_errors.append(std_cpu)
+    mem_values.append(avg_mem)
+    mem_errors.append(std_mem)
+
+legend_labels = ['Login request before installing the method', 'Login request after installing the method', 'After login request before installing the method', 'After login request after installing the method', 'After login confidential request after installing the method']
+
+# CPU使用率のグラフを描画 & 保存
+plot_and_save_graph(labels, cpu_values, cpu_errors, 'CPU Usage [%]', './evaluation/graph/cpu_usage_comparison_en.png', legend_labels)
+
+# メモリ使用量のグラフを描画 & 保存
+plot_and_save_graph(labels, mem_values, mem_errors, 'Memory Usage [MB]', './evaluation/graph/memory_usage_comparison_en.png', legend_labels)
---
+++ evaluation/scripts/gen_response_time_graph_en.py
@@ -0,0 +1,53 @@
+import csv
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+import math
+
+def calc_response_time_stats(filename):
+    response_times = []
+    with open(filename, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            response_times.append(float(row['Response Time']))
+    
+    mean = sum(response_times) / len(response_times)
+    variance = sum([(x - mean) ** 2 for x in response_times]) / len(response_times)
+    std_dev = math.sqrt(variance)
+    
+    return mean, std_dev
+
+# CSVファイルのパスを指定
+baseline_signin_csv = "./evaluation/data/performance/response_time/baseline/response_times_signin.csv"
+proposed_signin_csv = "./evaluation/data/performance/response_time/proposed/response_times_signin.csv"
+baseline_after_signin_csv = "./evaluation/data/performance/response_time/baseline/response_times_after_signin.csv"
+proposed_after_signin_csv = "./evaluation/data/performance/response_time/proposed/response_times_after_signin.csv"
+proposed_after_signin_confidential_csv = "./evaluation/data/performance/response_time/proposed/response_times_after_signin_confidential.csv"
+
+# CSVファイルから平均と標準偏差を計算
+mean_baseline_signin, std_baseline_signin = calc_response_time_stats(baseline_signin_csv)
+mean_proposed_signin, std_proposed_signin = calc_response_time_stats(proposed_signin_csv)
+mean_baseline_after_signin, std_baseline_after_signin = calc_response_time_stats(baseline_after_signin_csv)
+mean_proposed_after_signin, std_proposed_after_signin = calc_response_time_stats(proposed_after_signin_csv)
+mean_proposed_after_signin_confidential, std_proposed_after_signin_confidential = calc_response_time_stats(proposed_after_signin_confidential_csv)
+
+# グラフを描画
+labels = ['Before 1', 'After 1', 'Before 2', 'After 2', 'After 3']
+values = [mean_baseline_signin*1000, mean_proposed_signin*1000, mean_baseline_after_signin*1000, mean_proposed_after_signin*1000, mean_proposed_after_signin_confidential*1000]
+std_devs = [std_baseline_signin*1000, std_proposed_signin*1000, std_baseline_after_signin*1000, std_proposed_after_signin*1000, std_proposed_after_signin_confidential*1000]
+colors = ['gainsboro', 'darkgray', 'lightgray', 'grey', 'dimgray']
+
+legend_labels = ['Login request before installing the method', 'Login request after installing the method', 'After login request before installing the method', 'After login request after installing the method', 'After login confidential request after installing the method']
+
+plt.tick_params(labelsize=12)
+bars = plt.bar(labels, values, color=colors, yerr=std_devs, capsize=5)
+for bar, legend_label in zip(bars, legend_labels):
+    bar.set_label(legend_label)
+plt.ylabel('Response time [ms]', fontsize=14)
+plt.legend()
+
+# グラフをファイルとして保存
+output_filename = "./evaluation/graph/response_times_comparison_en.png"
+plt.savefig(output_filename, format="png", dpi=300)
+print(f"Graph saved to {output_filename}")
+
+plt.close()
